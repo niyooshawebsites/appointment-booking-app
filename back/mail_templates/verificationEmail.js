ff6fd27aa7a676469c8266b6e159da9988c63ff2
niyooshawebsites <niyooshawebsites@gmail.com>
2024-10-29 20:29:08 +0530

    email design v1

---
+++ back/mail_templates/verificationEmail.js
@@ -0,0 +1,13 @@
+const verificationEmailTemplate = `<div style={{}} fontfamily:="" "arial,="" sans-serif",="" maxwidth:="" 600,="" margin:="" "auto",="" padding:="" 20,="" border:="" "1px="" solid="" #ddd",="" borderradius:="" 8,="" backgroundcolor:="" "#f9f9f9"="" }}="">
+  <h2 style={{}} color:="" "#333"="" }}="">Hello there,</h2>
+  <p style={{}} fontsize:="" 16,="" color:="" "#555"="" }}="">
+    Please click the link below to verify your email address:
+  </p>
+  <a href="#" target="_blank" style={{}} display:="" "inline-block",="" margin:="" "20px="" 0",="" padding:="" "10px="" 20px",="" backgroundcolor:="" "#007bff",="" color:="" "#fff",="" textdecoration:="" "none",="" borderradius:="" 5="" }}="">Verify Email</a>
+  <p style={{}} fontsize:="" 14,="" color:="" "#555"="" }}="">
+    Thank you and regards,<br />
+    <strong>Team - ABS</strong>
+  </p>
+</div>`;
+
+module.exports = verificationEmailTemplate;
--- back/utils/mail.js
+++ back/utils/mail.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
+const verificationEmailTemplate = require;
 
 const sendverificationEmail = (email, subject, text) => {
   // creating the transporter
@@ -18,13 +19,17 @@ const sendverificationEmail = (email, subject, text) => {
     to: email,
     subject,
     html: `
-    Hello there,
-    <br> 
-    Please click the link to verify your email: <a href="${text}" target="_blank">Verify email</a>
-    <br> 
-    Thaks and regards,
-    <br>
-    Team - ABS
+      <div style={{}} fontfamily:="" "arial,="" sans-serif",="" maxwidth:="" 600,="" margin:="" "auto",="" padding:="" 20,="" border:="" "1px="" solid="" #ddd",="" borderradius:="" 8,="" backgroundcolor:="" "#f9f9f9"="" }}="">
+        <h2 style={{}} color:="" "#333"="" }}="">Hello there,</h2>
+        <p style={{}} fontsize:="" 16,="" color:="" "#555"="" }}="">
+          Please click the link below to verify your email address:
+        </p>
+        <a href="#" target="_blank" style={{}} display:="" "inline-block",="" margin:="" "20px="" 0",="" padding:="" "10px="" 20px",="" backgroundcolor:="" "#007bff",="" color:="" "#fff",="" textdecoration:="" "none",="" borderradius:="" 5="" }}="">Verify Email</a>
+        <p style={{}} fontsize:="" 14,="" color:="" "#555"="" }}="">
+          Thank you and regards,<br />
+          <strong>Team - ABS</strong>
+        </p>
+      </div>
     `,
   };
 
