241a6ae69a41d34c5eb40eea6f2cd68204b6fbf1
niyooshawebsites <niyooshawebsites@gmail.com>
2025-02-13 09:29:42 +0530

    changes

--- front/src/components/ResetPasswordForm.jsx
+++ front/src/components/ResetPasswordForm.jsx
@@ -39,45 +39,45 @@ const ResetPasswordForm = () => {
     } catch (err) {
       toast.error(err.response.data.msg);
     }
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700" htmlFor="email">
-            New Password
-          </label>
-          <input
-            type="password"
-            value={newPassword}
-            onChange={handleChangeNewPassword}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="New password"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-700" htmlFor="email">
-            Confirm New Password
-          </label>
-          <input
-            type="password"
-            onChange={handleChangeConfirmNewPassword}
-            value={confirmNewPassword}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Cofirm new password"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 rounded hover:bg-indigo-700 transition duration-200"
-        >
-          Reset Password
-        </button>
-      </form>
-    );
   };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label className="block text-gray-700" htmlFor="email">
+          New Password
+        </label>
+        <input
+          type="password"
+          value={newPassword}
+          onChange={handleChangeNewPassword}
+          className="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-300"
+          placeholder="New password"
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-gray-700" htmlFor="email">
+          Confirm New Password
+        </label>
+        <input
+          type="password"
+          onChange={handleChangeConfirmNewPassword}
+          value={confirmNewPassword}
+          className="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-300"
+          placeholder="Cofirm new password"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-indigo-600 text-white py-2 rounded hover:bg-indigo-700 transition duration-200"
+      >
+        Reset Password
+      </button>
+    </form>
+  );
 };
 
 export default ResetPasswordForm;
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,17 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     host: true, // Allows access from other devices on the network
     port: 5173, // Default port, you can change this if needed
